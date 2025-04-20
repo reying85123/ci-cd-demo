1b6a350d1a8de65ff7e51df2f72b4e50cd62efea
LeoHong <Leo266470@gmail.com>
2025-04-20 13:50:28 +0800

    test: test1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import TestView from './components/TestView.vue'
 
-const testString = ref('CI/CD Demo')
+const testString = ref('CI/CD Demo test1')
 </script>
 
 <template>
