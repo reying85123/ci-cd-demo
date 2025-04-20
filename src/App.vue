60973bd16b45a73ca6d7389a2761481326ccb68a
LeoHong <Leo266470@gmail.com>
2025-04-20 13:46:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+import TestView from './components/TestView.vue'
+
+const testString = ref('CI/CD Demo')
+</script>
+
+<template>
+  <TestView :msg="testString" />
+</template>
